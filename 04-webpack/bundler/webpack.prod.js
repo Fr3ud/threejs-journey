2dff551c50a2080def5b25d3d8d9a9dfb8c7b892
Fr3ud <oo.pavelko@gmail.com>
2021-08-08 20:49:14 +0300

    update indentation

--- 04-webpack/bundler/webpack.prod.js
+++ 04-webpack/bundler/webpack.prod.js
@@ -1,14 +1,14 @@
-const { merge } = require('webpack-merge')
+const {merge} = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = merge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
-    }
+  commonConfiguration,
+  {
+    mode: 'production',
+    plugins:
+      [
+        new CleanWebpackPlugin()
+      ]
+  }
 )
