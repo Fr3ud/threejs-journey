dbaa1fe441be205db75c063fb2520621410eaf88
Fr3ud <oo.pavelko@gmail.com>
2021-08-09 23:12:54 +0300

    add 05-transform-objects

---
+++ 05-transforms-objects/src/script.js
@@ -0,0 +1,35 @@
+import './style.css'
+import * as THREE from 'three'
+
+console.log(THREE)
+
+const canvas = document.querySelector('.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 'tomato' });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGL1Renderer({
+    canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
